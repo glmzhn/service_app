5eb201e58e9e07b22f3af07f516552564cb3f42d
glmzhn <galymzhan2007@gmail.com>
2024-04-14 00:50:01 +0500

    ORM-query optimization 2

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     client = serializers.CharField(source='client.company')
     email = serializers.CharField(source='client.user.email')
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client', 'email')
+        fields = ('id', 'plan_id', 'client', 'email', 'plan')
